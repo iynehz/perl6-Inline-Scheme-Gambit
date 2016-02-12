cec54ea022f0b3fb2b8c0fc4810e0a92e1a4d418
zhouzhen1 <zhouzhen1@gmail.com>
2016-02-12 11:08:02 +0800

    travis

--- Build.pm
+++ Build.pm
@@ -9,10 +9,23 @@ class Build is Panda::Builder {
     method build($dir) {
         my %vars = get-vars($dir);
 
-        %vars<GSC> = 'gsc-script';
-        %vars<LIBS> = '-lutil';
-        %vars<MYEXTLIB> = '/usr/lib/gambit-c/libgambit.a';
+        my $gsc_bin = (%*ENV<GSC> || q:x/which gsc-script/ || q:x/which gsc/).trim();
+        my $gsc_ver = (qq:x/$gsc_bin -v/).trim();
+        my $gsc_ver_int;
+        if ($gsc_ver ~~ /(\d+)\.(\d+)\.(\d+)/) {
+            $gsc_ver_int = ~$0 * 100000 + ~$1 * 1000 + ~$2;
+        } 
+        if (defined $gsc_ver_int) {
+            %vars<GSC_VER> = $gsc_ver_int;
+        } else {
+            die "unable to determine gsc version";
+        }
+
+        %vars<GSC> = $gsc_bin;
+        %vars<LIBS> = %*ENV<LIBS> || '-lgambc';
+        %vars<MYEXTLIB> = %*ENV<MYEXTLIB> || '';
         %vars<gambithelper> = $*VM.platform-library-name('gambithelper'.IO);
+        say %vars;
 
         mkpath "$dir/resources/libraries";
 
